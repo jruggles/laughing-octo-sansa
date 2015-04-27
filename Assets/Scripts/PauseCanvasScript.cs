1787441199bd96a1a646cdf6f050796781397cd6
Jonathan Ruggles <briarerios@gmail.com>
2015-04-26 19:42:02 -0500

    Pause and Quit Menu in place

---
+++ Assets/Scripts/PauseCanvasScript.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseCanvasScript : MonoBehaviour {
+
+	public Canvas PauseCanvas;
+	public Canvas QuitCanvas;
+
+	private bool toggle = false;
+	private int count = 0;
+	private int play = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Escape))
+		{
+			Time.timeScale = (play++ % 2);
+			toggle = !toggle;
+			PauseCanvas.enabled = toggle;
+		}
+	}
+
+	public void loadLevel()
+	{
+		Application.LoadLevel(2);
+		Time.timeScale = (play++ % 2);
+	}
+	
+	public void quit()
+	{
+		toggle = !toggle;
+		PauseCanvas.enabled = toggle;
+		QuitCanvas.enabled = !toggle;
+	}
+
+	public void quitConfirm()
+	{
+		Application.Quit();
+		Debug.Log("you quit");
+	}
+
+	public void continueGame()
+	{
+		Time.timeScale = (play++ % 2);
+		toggle = !toggle;
+		PauseCanvas.enabled = toggle;
+	}
+}
---
+++ Assets/Scripts/StartTimerScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class StartTimerScript : MonoBehaviour {
+
+	public Canvas TimerCanvas;
+	public Canvas HUD;
+	public Text TimerText;
+
+	// Use this for initialization
+	void Start () {
+		//Time.timeScale = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	
+		if (Time.deltaTime < 1) {
+			TimerText.text = ("3");
+		} else if (Time.deltaTime < 2 && Time.deltaTime > 1) {
+			TimerText.text = ("2");
+		}else{
+			TimerText.text = ("1");
+		}
+	
+
+		if (Time.deltaTime == 3) {
+			TimerCanvas.enabled = false;
+			HUD.enabled = true;
+			Time.timeScale = 1;
+		}
+	}
+}
