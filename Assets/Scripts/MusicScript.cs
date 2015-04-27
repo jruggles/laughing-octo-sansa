f079b49f8343e41e8f719578123215d62f421f42
mackHale <mack@neals-mbp.ok.cox.net>
2015-04-27 05:20:01 -0500

    Sound and Karts
    I added music/sound effects. I also added two more karts and two more
    “characters”

---
+++ Assets/Scripts/MusicScript.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicScript : MonoBehaviour {
+
+	private bool paused = false;
+	public AudioSource music;
+	public AudioSource carSound;
+
+	// Use this for initialization
+	void Start () {
+		music.Play ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Escape) && !paused) 
+		{
+			music.Pause ();
+			paused = true;
+		} 
+		else if (Input.GetKeyDown (KeyCode.Escape) && paused) {
+			music.UnPause ();
+			paused = false;
+		}
+
+		if (Input.GetKeyDown (KeyCode.UpArrow) && !paused) 
+		{
+			if(!carSound.isPlaying)
+			{
+				carSound.Play ();
+			}
+		}
+		if (Input.GetKeyUp (KeyCode.UpArrow) && !paused) 
+		{
+			if (carSound.isPlaying) 
+			{
+				carSound.Stop ();
+			}
+		}
+	
+	}
+}
